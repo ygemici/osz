1a935a14dd0a048b9218e02cbbec8b80e4c80df5
bzt <zoltan.baldaszti@gmail.com>
2016-12-26 05:45:45 +0000

    Lot of things, messaging and cooperative multitasking

--- tools/drivers.sh
+++ tools/drivers.sh
@@ -2,15 +2,7 @@
 
 out="../../../../bin/root/etc/sys/drivers"
 a=$1
-if [ "${a%/*}" == "fs" ]
-then
-    echo "*	$1" >>$out
-fi
-
+[ "${a%/*}" == "fs" ] && echo "*	$1" >>$out
 [ ! -f devices ] && exit
-
-cat devices | while read line
-do
-	echo "$line	$1"
-done >>$out
+cat devices | while read line; do echo "$line	$1"; done >>$out
 
