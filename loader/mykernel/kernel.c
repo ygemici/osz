381b818715fda2d9b3f7d3e5c6d792a1a279124c
bzt <zoltan.baldaszti@gmail.com>
2017-10-13 23:52:54 +0200

    VFS stuff

--- loader/mykernel/kernel.c
+++ loader/mykernel/kernel.c
@@ -8,6 +8,9 @@
  *
  */
 
+/* function to display a string, see below */
+void puts(char *s);
+
 /* we don't assume stdint.h exists */
 typedef short int           int16_t;
 typedef unsigned char       uint8_t;
@@ -19,9 +22,12 @@ typedef unsigned long int   uint64_t;
 
 /* imported virtual addresses, see linker script */
 extern BOOTBOOT bootboot;           // see bootboot.h
-extern unsigned char *environment;  // configuration, key=value pairs
+extern unsigned char *environment;  // configuration, UTF-8 text key=value pairs
 extern uint8_t fb;                  // linear framebuffer mapped
 
+/******************************************
+ * Entry point, called by BOOTBOOT Loader *
+ ******************************************/
 void _start()
 {
     int x, y, s=bootboot.fb_scanline, w=bootboot.fb_width, h=bootboot.fb_height;
@@ -31,10 +37,47 @@ void _start()
     for(x=0;x<w;x++) { *((uint32_t*)(&fb + s*(h/2)+x*4))=0x00FFFFFF; }
 
     // red, green, blue boxes in order
-    for(y=0;y<20;y++) { for(x=0;x<20;x++) { *((uint32_t*)(&fb + s*(y+10) + (x+10)*4))=0x00FF0000; } }
-    for(y=0;y<20;y++) { for(x=0;x<20;x++) { *((uint32_t*)(&fb + s*(y+10) + (x+40)*4))=0x0000FF00; } }
-    for(y=0;y<20;y++) { for(x=0;x<20;x++) { *((uint32_t*)(&fb + s*(y+10) + (x+70)*4))=0x000000FF; } }
+    for(y=0;y<20;y++) { for(x=0;x<20;x++) { *((uint32_t*)(&fb + s*(y+20) + (x+20)*4))=0x00FF0000; } }
+    for(y=0;y<20;y++) { for(x=0;x<20;x++) { *((uint32_t*)(&fb + s*(y+20) + (x+50)*4))=0x0000FF00; } }
+    for(y=0;y<20;y++) { for(x=0;x<20;x++) { *((uint32_t*)(&fb + s*(y+20) + (x+80)*4))=0x000000FF; } }
+
+    // say hello
+    puts("Hello from a simple BOOTBOOT kernel");
 
     // hang for now
     while(1);
+}
+
+/* font to say hello */
+typedef struct {
+    uint32_t magic;
+    uint32_t version;
+    uint32_t headersize;
+    uint32_t flags;
+    uint32_t numglyph;
+    uint32_t bytesperglyph;
+    uint32_t height;
+    uint32_t width;
+    uint8_t glyphs;
+} __attribute__((packed)) psf2_t;
+extern volatile unsigned char _binary_font_psf_start;
+
+/* simple function to display text on screen */
+void puts(char *s)
+{
+    psf2_t *font = (psf2_t*)&_binary_font_psf_start;
+    int x,y,kx=0,line,mask,offs;
+    int bytesperline=(font->width+7)/8;
+    while(*s) {
+        unsigned char *glyph = (unsigned char*)&_binary_font_psf_start + font->headersize + 
+            (*s>0&&*s<font->numglyph?*s:0)*font->bytesperglyph;
+        offs = (kx * (font->width+1) * 4);
+        for(y=0;y<font->height;y++){
+            line=offs; mask=1<<(font->width-1);
+            for(x=0;x<font->width;x++)
+                {*((uint32_t*)((uint64_t)&fb+line))=((int)*glyph) & (mask)?0xFFFFFF:0; mask>>=1; line+=4;}
+            *((uint32_t*)((uint64_t)&fb+line))=0; glyph+=bytesperline; offs+=bootboot.fb_scanline;
+        }
+        s++; kx++;
+    }
 }
